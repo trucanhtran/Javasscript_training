da955208a8a472ed68b858babf3927e48d211856
DESKTOP-1F2SET5\TTC <anhttt18411@st.uel.edu.vn>
2020-08-03 22:13:28 +0700

    get object

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function myFunction() {
-  alert("Hello Word!");
-}
+var person = {
+  firstName: "John",
+  lastname: "Doe",
+  language: "en",
+  get lang() {
+    return this.language;
+  },
+};
+document.getElementById("demo").innerHTML = person.lang;
