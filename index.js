0e14c10910c323c326f481cfa59aea467f7d4e7e
DESKTOP-1F2SET5\TTC <anhttt18411@st.uel.edu.vn>
2020-08-03 22:34:42 +0700

    add get lang ()

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var person = {
   firstName: "John",
   lastname: "Doe",
+  language: "en",
   fullname: function () {
     return this.firstName + " " + this.lastname;
   },
   get fullname1() {
     return this.firstName + " " + this.lastname;
   },
+  get lang() {
+    return this.language.toUpperCase();
+  },
 };
 document.getElementById("demo").innerHTML =
-  person.fullname() + " " + person.fullname1;
+  person.fullname() + " " + person.fullname1 + " " + person.lang;
