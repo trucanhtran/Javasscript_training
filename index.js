590f7c31c8c43a09832ef4d9ec3d817dae10eea0
DESKTOP-1F2SET5\TTC <anhttt18411@st.uel.edu.vn>
2020-08-04 23:04:03 +0700

    using the prototype property

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ function Person(first, last, age, eye) {
   this.age = age;
   this.eyecolor = eye;
 }
-Person.prototype.nationality = "English";
+Person.prototype.name = function () {
+  return this.firstname + " " + this.lastname;
+};
 var myFather = new Person("John", "Doe", 50, "blue");
 var myMother = new Person("Sally", "Rally", 48, "green");
 document.getElementById("demo").innerHTML =
   "My father is " +
+  myFather.name() +
+  "<br/>" +
+  "He is " +
   myFather.age +
   ". My mother is " +
-  myMother.age +
-  "<br/>" +
-  "My father is " +
-  myFather.nationality +
-  ". My mother is " +
-  myMother.nationality;
+  myMother.age;
