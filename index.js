f5f54c26859dc4c3c986e4462308499b81967dd7
DESKTOP-1F2SET5\TTC <anhttt18411@st.uel.edu.vn>
2020-08-07 23:29:24 +0700

    pass a parameter into the " present()" method

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ class Car {
   constructor(brand) {
     this.carname = brand;
   }
-  present() {
-    return "I have a " + this.carname;
+  present(x) {
+    return x + ", I have a " + this.carname;
   }
 }
 mycar = new Car("Ford");
-document.getElementById("demo").innerHTML = mycar.present();
+document.getElementById("demo").innerHTML = mycar.present("Hello");
