37d83f15f48efd7274d88b297fab0080621c11ca
DESKTOP-1F2SET5\TTC <anhttt18411@st.uel.edu.vn>
2020-08-07 23:26:45 +0700

    add method named "present'

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ class Car {
   constructor(brand) {
     this.carname = brand;
   }
+  present() {
+    return "I have a " + this.carname;
+  }
 }
 mycar = new Car("Ford");
-document.getElementById("demo").innerHTML = mycar.carname;
+document.getElementById("demo").innerHTML = mycar.present();
