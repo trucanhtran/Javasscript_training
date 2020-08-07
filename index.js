3c8f5728d034e9f1d64ef273c075c513216e1cfc
DESKTOP-1F2SET5\TTC <anhttt18411@st.uel.edu.vn>
2020-08-07 23:24:38 +0700

    add class car

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-function Person(first, last, age, eye) {
-  this.firstname = first;
-  this.lastname = last;
-  this.age = age;
-  this.eyecolor = eye;
+class Car {
+  constructor(brand) {
+    this.carname = brand;
+  }
 }
-Person.prototype.name = function () {
-  return this.firstname + " " + this.lastname;
-};
-var myFather = new Person("John", "Doe", 50, "blue");
-var myMother = new Person("Sally", "Rally", 48, "green");
-document.getElementById("demo").innerHTML =
-  "My father is " +
-  myFather.name() +
-  "<br/>" +
-  "He is " +
-  myFather.age +
-  ". My mother is " +
-  myMother.age;
+mycar = new Car("Ford");
+document.getElementById("demo").innerHTML = mycar.carname;
